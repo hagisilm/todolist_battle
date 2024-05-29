50a550114abb45e7585b5bbaf66dcd5fb8bdc01d
KIM BYEONG JU <100591433+hagisilm@users.noreply.github.com>
2024-05-29 09:51:18 +0900

    feat: FCMconfig 추가

---
+++ src/main/java/com/hci/todolist_battle/battle/config/FCMConfig.java
@@ -0,0 +1,45 @@
+package com.hci.todolist_battle.battle.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import lombok.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+
+@Configuration
+public class FCMConfig {
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException {
+        ClassPathResource resource = new ClassPathResource("firebase/fcm-test-9c370-firebase-adminsdk-agxt7-981c0f9a64.json");
+
+        InputStream refreshToken = resource.getInputStream();
+
+        FirebaseApp firebaseApp = null;
+        List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
+
+        if (firebaseAppList != null && !firebaseAppList.isEmpty()) {
+            for (FirebaseApp app : firebaseAppList) {
+                if (app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
+                    firebaseApp = app;
+                }
+            }
+        } else {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(refreshToken))
+                    .build();
+
+            firebaseApp = FirebaseApp.initializeApp(options);
+        }
+
+        assert firebaseApp != null;
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
+}
